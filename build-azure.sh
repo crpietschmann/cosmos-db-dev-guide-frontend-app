d53e38d5ad99f78ed60f626d07c9b259342cd1a4
Chris Pietschmann <chris@build5nines.com>
2023-12-28 16:17:38 -0500

    initial commit

---
+++ build-azure.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# https://learn.microsoft.com/en-us/azure/app-service/configure-language-nodejs?pivots=platform-linux#customize-build-automation
+
+touch dist/assets/apiUrl.js
+echo "var apiUrl = '${APPSETTING_API_ENDPOINT}';" > dist/assets/apiUrl.js
