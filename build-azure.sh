f9e93ca1bc5c0288f7ab9b0ede10c8d867df1033
Chris Pietschmann <chris@build5nines.com>
2023-12-28 16:26:53 -0500

    Update build-azure.sh

--- build-azure.sh
+++ build-azure.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+# This is the NPM "build:azure" build script for Azure App Service Build Service to use
+
 # https://learn.microsoft.com/en-us/azure/app-service/configure-language-nodejs?pivots=platform-linux#customize-build-automation
 
+# Add environment variable to js file for reference
 touch dist/assets/apiUrl.js
 echo "var apiUrl = '${APPSETTING_API_ENDPOINT}';" > dist/assets/apiUrl.js
 
-cp -r "dist/*" "./"
+# Move the built site to the root directory
+cp -r ./dist/* ./
