657318d80373b044d8fdd02974801e10cec9c99c
Chris Pietschmann <chris@build5nines.com>
2024-01-08 22:43:34 -0500

    Update Chat.tsx

--- src/pages/chat/Chat.tsx
+++ src/pages/chat/Chat.tsx
@@ -238,7 +238,7 @@ const Chat = () => {
                     <div className={styles.chatInput}>
                         <QuestionInput
                             clearOnSend
-                            placeholder="Type a new question (e.g. does my plan cover annual eye exams?)"
+                            placeholder="Type a new question (e.g. what is product FR-R92B-58?)"
                             disabled={isLoading}
                             onSend={question => makeApiRequest(question)}
                         />
